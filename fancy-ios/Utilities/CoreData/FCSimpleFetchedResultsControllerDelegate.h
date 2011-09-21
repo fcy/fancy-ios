f7032bedad32f0d2df0ca3d0d3ba36697ca3df90
Felipe Cypriano <fmcypriano@gmail.com>
2011-09-21 14:24:28 -0300

    Create FCSimpleFetchedResultsControllerDelegate

---
+++ fancy-ios/Utilities/CoreData/FCSimpleFetchedResultsControllerDelegate.h
@@ -0,0 +1,49 @@
+//    Copyright 2011 Felipe Cypriano
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+/**
+ * @file FCSimpleFetchedResultsControllerDelegate
+ * A simple, yet useful, implementation of NSFetchedResultsControllerDelegate
+ *
+ * @author Felipe Cypriano
+ * @date 2011
+ */
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ * @brief A delegate to handle NSFetchedResultsChangeInsert, NSFetchedResultsChangeDelete and NSFetchedResultsChangeMove
+ *
+ * <p>A implementation of NSFetchedResultsControllerDelegate that handles
+ * the basics operations insert, delete and move. This class doesn't retain itself, you should maintain ownership on your calling code.</p>
+ *
+ * <p>Use it when you don't need any special cases to handle
+ * changes from NSManagedObjectContext. Example:</p>
+ *
+ * <code>
+     NSFetchedResultsController *fetchedController = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:moc sectionNameKeyPath:nil cacheName:nil];
+     FCSimpleFetchedResultsControllerDelegate *delegate = [[FCSimpleFetchedResultsControllerDelegate alloc] initWithTableView:[self tableView]];
+     fetchedController.delegate = delegate;
+ * </code>
+ */
+@interface FCSimpleFetchedResultsControllerDelegate : NSObject <NSFetchedResultsControllerDelegate>
+
+@property (nonatomic, assign, readonly) UITableView *tableView; 
+@property (nonatomic) UITableViewRowAnimation insertAnimation;
+@property (nonatomic) UITableViewRowAnimation deleteAnimation;
+
+- (id)initWithTableView:(UITableView *)tableView;
+
+@end
---
+++ fancy-ios/Utilities/CoreData/FCSimpleFetchedResultsControllerDelegate.m
@@ -0,0 +1,64 @@
+//    Copyright 2011 Felipe Cypriano
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#import "FCSimpleFetchedResultsControllerDelegate.h"
+
+@implementation FCSimpleFetchedResultsControllerDelegate
+
+@synthesize tableView;
+@synthesize insertAnimation;
+@synthesize deleteAnimation;
+
+- (id)initWithTableView:(UITableView *)newTableView {
+    self = [super init];
+    if (self) {
+        tableView = newTableView;
+        insertAnimation = UITableViewRowAnimationTop;
+        deleteAnimation = insertAnimation;
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark NSFetchedResultsControllerDelegate
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [[self tableView] beginUpdates];    
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [[self tableView] insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:insertAnimation];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [[self tableView] deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:deleteAnimation];
+            break;
+        case NSFetchedResultsChangeMove:
+            [[self tableView] deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:deleteAnimation];
+            [[self tableView] insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:insertAnimation];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [[self tableView] endUpdates];
+}
+
+@end

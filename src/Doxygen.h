f3d9ddd9f68e9bf45fbb7719326bcfe8036cfd51
Felipe Cypriano <felipe@cypriano.me>
2012-05-31 19:18:06 -0300

    Update project requirements

--- src/Doxygen.h
+++ src/Doxygen.h
@@ -18,7 +18,7 @@
  * The list of dependencies is listed as comments in the header files. 
  *
  * <h2>Requirements</h2>
- * - Xcode: 4.0
- * - Minumum iOS version: 4.0
+ * - Minumum iOS version: 5.0
+ * - ARC
  * - See the Xcode project to see the list of frameworks used. As of this moment there's no external frameworks or libraries in the project. 
  */

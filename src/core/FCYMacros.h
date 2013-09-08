be8fa932834581138c76bb3fdd011651707f3745
Felipe Cypriano <felipe@cypriano.me>
2013-09-08 16:21:22 -0300

    Add macro FCYSuppressWarning(warningName, code)

--- src/core/FCYMacros.h
+++ src/core/FCYMacros.h
@@ -22,3 +22,30 @@
  * @param name the name of the new strong variable
  */
 #define FCYStrongifySelfInto(name) typeof(self) __strong name = _fcy_weakSelf
+
+// ## Private Macros ##
+// These __STR_BUILD* macros are a trick to build a string literal to _Pragma
+// Source: http://stackoverflow.com/a/8724905/335974
+#define __STR_BUILD0(name) #name
+#define __STR_BUILD1(name) __STR_BUILD0(clang diagnostic ignored name)
+#define __STR_BUILD2(name) __STR_BUILD1(#name)
+// ## Private Macros ##
+
+/**
+ * Creates a `clang diagnostics push/ignore/pop` around the code
+ *
+ * Usage example:
+ *     FCYSupressWarning(-Wunused-value, {
+ *         [[NSObject alloc] init];
+ *     });
+ *
+ * @param warningName this is the warning to ignore. E.g.: -Wunused-value
+ * @param code the statement(s) that the ignore will be around
+ */
+#define FCYSuppressWarning(warningName, code) \
+_Pragma("clang diagnostic push") \
+_Pragma(__STR_BUILD2(warningName)) \
+do { \
+    code \
+} while (0); \
+_Pragma("clang diagnostic pop")
